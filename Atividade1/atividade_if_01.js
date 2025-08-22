54ad0a02c21502fa111bf8763c8f02cc52379266
raphaellyta <rafaelly91@gmail.com>
2025-08-21 21:10:11 -0300

    oi

---
+++ Atividade1/atividade_if_01.js
@@ -0,0 +1,18 @@
+import PromptSync from "prompt-sync"
+const pront = PromptSync()
+
+let velocidade = Number( pront('Digite a velocidade permitida na via: '))
+let velocidade_media = Number(pront ('Digite a velocidade media do Veiculo: '))
+
+let valor_max = (velocidade*0.10) +velocidade
+
+if(velocidade_media > velocidade && velocidade_media<valor_max){
+
+    console.log(`Atenção: acima do permitido, multa leve!`)
+}else if(velocidade_media>valor_max){
+
+    console.log(`Atenção: acima do permitido, multa grave!`)
+
+}else console.log(`Velocidade dentro do limite`)
+
+
