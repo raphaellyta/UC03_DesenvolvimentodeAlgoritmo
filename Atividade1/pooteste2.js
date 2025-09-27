4f519a33097d3d28b3246a8f505e461864db4534
RAFAELA SANTOS DA COSTA <a92313599@rn.senac.local>
2025-09-26 21:16:01 -0300

    Atualização da Segunda Questão

--- Atividade1/pooteste2.js
+++ Atividade1/pooteste2.js
@@ -4,11 +4,19 @@ const prompt = PromptSync()
 
 
 let clin1 = new ContaBancaria()
-clin1.nomeTitular = prompt("Digite o nome: ")
+clin1.nomeTitular = (prompt("Digite o nome: "))
+clin1.numeroAgencia = (prompt("Digite o numero de sua agencia:"))
 clin1.numeroConta= prompt("Digite o numero de sua conta ")
+clin1.depositar(prompt(`Digite o valor que vai deposição`))
+clin1.sacar(prompt(`Digite o valor que vai  Sacar`))
+clin1.imprimir()
 
 
 
 let clin2 = new ContaBancaria()
-clin2.nomeTitular = prompt("Digite o nome: ")
+clin2.nomeTitular = (prompt("Digite o nome: "))
+clin2.numeroAgencia = (prompt("Digite o numero de sua agencia:"))
 clin2.numeroConta= prompt("Digite o numero de sua conta ")
+clin2.depositar(prompt(`Digite o valor que vai deposição`))
+clin2.sacar(prompt(`Digite o valor que vai  Sacar`)) 
+clin2.imprimir()
